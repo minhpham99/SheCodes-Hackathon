e61be37d293752c28637f4d4b5156ab5bbebe6e5
Minh Pham <sc17mp@leeds.ac.uk>
2018-07-14 21:26:39 +0700

    Trying to make firebase authentication

---
+++ firebase.js
@@ -0,0 +1,65 @@
+function signup() {
+    var email = document.getElementsByClassName('guestEmail').value;
+    var password = document.getElementsByClassName('guestPassword').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]]]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+    // [END createwithemail]
+  }
+
+
+  function signin() {
+      var email = document.getElementsByClassName('guestEmail').value;
+      var password = document.getElementsByClassName('guestPassword').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START authwithemail]
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        document.getElementsByClassName('signup').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authwithemail]
+    document.getElementsByClassName('signup').disabled = true;
+  }
+
+  function initApp() {
+    document.getElementById('signin').addEventListener('click', signin, false);
+    document.getElementById('signup').addEventListener('click', signup, false);
+  }
