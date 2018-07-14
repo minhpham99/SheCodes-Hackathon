5c73528dff4c9cf46a35caa7cc21b80c37b56746
Minh Pham <sc17mp@leeds.ac.uk>
2018-07-14 22:51:27 +0700

    Finale touches

--- firebase.js
+++ firebase.js
@@ -1,45 +1,41 @@
 function signUp() {
-    var email = document.getElementsByClassName('guestEmail')[0].value;
-    var password = document.getElementsByClassName('guestPassword')[0].value;
+    var email = document.getElementsByClassName("guestEmail")[0].value;
+    var password = document.getElementsByClassName("guestPassword")[0].value;
     if (email.length < 4) {
-      alert('Please enter an email address.');
+      alert("Địa chỉ email không hợp lệ");
       return;
     }
     if (password.length < 4) {
-      alert('Please enter a password.');
+      alert("Password không hợp lệ");
       return;
     }
     // Sign in with email and pass.
     // [START createwithemail]]]
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    
-
-            // Handle Errors here.
+        // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       // [START_EXCLUDE]
-      if (errorCode == 'auth/weak-password') {
-        alert('The password is too weak.');
+      if (errorCode == "auth/weak-password") {
+        alert("Password quá yếu");
       } else {
         alert(errorMessage);
       }
       console.log(error);
       // [END_EXCLUDE]
     });
-
     // [END createwithemail]
   }
 
-
   function signIn() {
-      var email = document.getElementsByClassName('guestEmail').value;
-      var password = document.getElementsByClassName('guestPassword').value;
+      var email = document.getElementsByClassName("guestEmail").value;
+      var password = document.getElementsByClassName("guestPassword").value;
       if (email.length < 4) {
-        alert('Please enter an email address.');
+        alert("Địa chỉ email không hợp lệ");
         return;
       }
       if (password.length < 4) {
-        alert('Please enter a password.');
+        alert("Password không hợp lệ");
         return;
       }
       // Sign in with email and pass.
@@ -49,20 +45,21 @@ function signUp() {
         var errorCode = error.code;
         var errorMessage = error.message;
         // [START_EXCLUDE]
-        if (errorCode === 'auth/wrong-password') {
-          alert('Wrong password.');
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password.");
         } else {
           alert(errorMessage);
         }
         console.log(error);
-        document.getElementsByClassName('signup').disabled = false;
+        document.getElementsByClassName("signup").disabled = false;
+        windows.location("homepage.txt");
         // [END_EXCLUDE]
       });
       // [END authwithemail]
-    document.getElementsByClassName('signup').disabled = true;
+    document.getElementsByClassName("signup").disabled = true;
   }
 
   function initApp() {
-    document.getElementsByClassName('signin').addEventListener('click', signIn, false);
-    document.getElementsByClassName('signup').addEventListener('click', signUp, false);
+    document.getElementsByClassName("signin").addEventListener("click", signIn, false);
+    document.getElementsByClassName("signup").addEventListener("click", signUp, false);
   }
