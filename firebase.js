2f1d589cf11f6f4447f1dd42b14a5115b7f17d81
Minh Pham <sc17mp@leeds.ac.uk>
2018-07-24 22:45:50 +0700

    Finished the project in time

--- firebase.js
+++ firebase.js
@@ -1,65 +1,56 @@
 function signUp() {
-    var email = document.getElementsByClassName("guestEmail")[0].value;
-    var password = document.getElementsByClassName("guestPassword")[0].value;
+  var email = document.getElementsByClassName('guestEmail')[0].value;
+  var password = document.getElementsByClassName('guestPassword')[0].value;
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+
+}
+
+
+function signIn() {
+    var email = document.getElementsByClassName('guestEmail').value;
+    var password = document.getElementsByClassName('guestPassword').value;
     if (email.length < 4) {
-      alert("Địa chỉ email không hợp lệ");
+      alert('Please enter an email address.');
       return;
     }
     if (password.length < 4) {
-      alert("Password không hợp lệ");
+      alert('Please enter a password.');
       return;
     }
-    // Sign in with email and pass.
-    // [START createwithemail]]]
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
-      // [START_EXCLUDE]
-      if (errorCode == "auth/weak-password") {
-        alert("Password quá yếu");
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
       } else {
         alert(errorMessage);
       }
       console.log(error);
-      // [END_EXCLUDE]
+      document.getElementsByClassName('signup').disabled = false;
+      windows.location("homepage.html");
     });
-    // [END createwithemail]
-  }
+  document.getElementsByClassName('signup').disabled = true;
+}
 
-  function signIn() {
-      var email = document.getElementsByClassName("guestEmail").value;
-      var password = document.getElementsByClassName("guestPassword").value;
-      if (email.length < 4) {
-        alert("Địa chỉ email không hợp lệ");
-        return;
-      }
-      if (password.length < 4) {
-        alert("Password không hợp lệ");
-        return;
-      }
-      // Sign in with email and pass.
-      // [START authwithemail]
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode === "auth/wrong-password") {
-          alert("Wrong password.");
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        document.getElementsByClassName("signup").disabled = false;
-        windows.location("homepage.txt");
-        // [END_EXCLUDE]
-      });
-      // [END authwithemail]
-    document.getElementsByClassName("signup").disabled = true;
-  }
-
-  function initApp() {
-    document.getElementsByClassName("signin").addEventListener("click", signIn, false);
-    document.getElementsByClassName("signup").addEventListener("click", signUp, false);
-  }
+function initApp() {
+  document.getElementsByClassName('signin').addEventListener('click', signIn, false);
+  document.getElementsByClassName('signup').addEventListener('click', signUp, false);
+}
